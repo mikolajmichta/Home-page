5fc0cb607b185cd328070c4d051a984ac23fa880
mikolajmichta <hansolo92@wp.pl>
2018-06-14 18:36:58 +0200

    audio removed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -67,7 +67,7 @@ function makeVapor() {
     $("a.nav-item").toggleClass("btn-outline-secondary btn-outline-primary ");
     $(".footer").toggleClass("bg-light footer-alt");
     $(`#fiji`).on(`click`, makeBoring);
-    $("audio").css("visibility", "visible")
+    
 }
 
 function makeBoring() {
@@ -80,8 +80,7 @@ function makeBoring() {
     $("a.nav-item").toggleClass("btn-outline-secondary btn-outline-primary ");
     $(".footer").toggleClass("bg-light footer-alt");
     $(`#fiji`).on(`click`, makeVapor);
-    $("audio").css("visibility", "hidden")
-    $('#music').trigger("pause");
+   
 }
 
 
